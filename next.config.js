45f995ac2f7b59b9352f47b9b7a2eab10973ddf6
Triệu <trieuthai1395@gmail.com>
2026-06-10 15:44:17 +0700

    fix output export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',           // Thử cách này
   images: {
     unoptimized: true,
   },
-  trailingSlash: false,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
